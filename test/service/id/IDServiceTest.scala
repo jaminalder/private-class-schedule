bf7531311aeaae9058d66be686383fb55dba18c3
jaminalder <bilibu@blu.com>
2013-04-10 23:18:15 +0200

    insert student - not completed yet

--- test/service/id/IDServiceTest.scala
+++ test/service/id/IDServiceTest.scala
@@ -17,13 +17,13 @@ class IDServiceTest extends Specification {
     "generate UUIDs" in new WithServer {
 
       val requestHolder = WS.url("http://localhost:19001/api/id/generate")
-      val response1: Response = await(requestHolder.get)
+      val response1: Response = await(requestHolder.get())
       response1.status must equalTo(OK)
-      val response2: Response = await(requestHolder.get)
+      val response2: Response = await(requestHolder.get())
       response2.status must equalTo(OK)
-      response1.body.size mustEqual 36
-      response2.body.size mustEqual 36
-      response1.body mustNotEqual response2.body
+      (response1.json \ "_id").as[String].size mustEqual 36
+      (response2.json \ "_id").as[String].size mustEqual 36
+      response1.json mustNotEqual response2.json
 
     }
 
