395c0874b0f40544ff84ef6cc8856ed27e68894c
ageser <ag@fake.com>
2013-05-26 09:51:07 +0200

    Added first version of calendar - not completed yet, calender english,  student list does not display

--- public/angular-frontend/app/js/app.js
+++ public/angular-frontend/app/js/app.js
@@ -2,4 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('pcs', ['pcs.routes', 'student'])
+angular.module('pcs', ['pcs.routes', 'student', 'calendar']) ;
--- public/angular-frontend/app/js/routes.js
+++ public/angular-frontend/app/js/routes.js
@@ -3,5 +3,7 @@
 angular.module('pcs.routes', []).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/student', {templateUrl: 'view/student/studentListAndFormPage.html', controller:StudentCtrl});
+    $routeProvider.when('/calendar', {templateUrl: 'view/calendar/calendar.html', controller:CalendarCtrl});
+    $routeProvider.when('/calendarStudentList', {templateUrl: 'view/calendar/calendarStudentList.html', controller:CalendarCtrl});
     $routeProvider.otherwise({redirectTo: '/student'});
   }]);
