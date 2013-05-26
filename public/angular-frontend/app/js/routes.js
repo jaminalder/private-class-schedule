997c6c8ae8933f8234ace0d87c67bd6f1e06b615
ageser <ag@fake.com>
2013-05-26 08:38:37 +0200

    Removed not used html files

--- public/angular-frontend/app/js/routes.js
+++ public/angular-frontend/app/js/routes.js
@@ -3,6 +3,5 @@
 angular.module('pcs.routes', []).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/student', {templateUrl: 'view/student/studentListAndFormPage.html', controller:StudentCtrl});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html'});
     $routeProvider.otherwise({redirectTo: '/student'});
   }]);
