7847846d523a69ff81eb2e83344bc8e4e95576b8
ageser <ag@fake.com>
2013-06-29 14:28:02 +0200

    Edit lesson implemented

--- public/angular-frontend/app/js/controllers/lesson.js
+++ public/angular-frontend/app/js/controllers/lesson.js
@@ -44,56 +44,23 @@ function($scope, $resource, $filter, UUIDService, LessonService, UserService, le
 //    (date[, format]   ('date', 'format:dd.MM.YYYY HH:mm')
 
     $scope.saveLesson = function () {
-
         console.log('lessonForm: ' + JSON.stringify($scope.lessonForm));
-        console.log('lessonForm typeof start: ' + typeof $scope.lessonForm.start);
-
-     // testing of date output
-        var startDateString = new Date(angular.copy($scope.lessonForm.start))   ;
-        console.log('startDateString ' + typeof startDateString + ' : ' + startDateString);
-        var startDate = Date.parse(startDateString);
-        console.log('startDate ' + typeof startDate + ' : ' + startDate);
-     // end of date output testing
         var lesson = $filter('date')(angular.copy($scope.lessonForm));
-     // output for lesson
-        var lessonStart = new Date(lesson.start);
-        console.log('lessonStart = ' + lessonStart)     ;
-        console.log('lesson ' + typeof lesson  + ' + ' + typeof lesson.start + ' start: ' + lesson.start + ' end: ' + lesson.end);
-        console.log('lesson ' + typeof lesson  + ' + ' + typeof lesson.start + ' start: ' + $filter('date', 'format: yyyy-MM-dd HH:mm')(lesson.start) + ' end: ' + $filter('date')(lesson.end));
-        console.log('lesson ' + typeof lesson  + ' + ' + typeof lesson.start + ' start: ' + new Date(lesson.start) + ' end: ' + $filter('date')(lesson.end));
-     // end of output for lesson
-
-        console.log('lesson to save on create: ' + JSON.stringify(lesson));
-
+        console.log('lesson to save: ' + JSON.stringify(lesson));
         LessonService.save(lesson);
         $scope.lessons[$scope.activeLessonIndex] = lesson;
-
         $scope.resetLessonForm();
         $scope.hideLessonDetail();
     };
 
     $scope.editLesson = function(lessonToEdit, index) {
-        console.log('editLesson ' + typeof lessonToEdit.start + ' start ' + lessonToEdit.start )
-        var lessonStart = new Date(lessonToEdit.start);
-        var lessonStartString = lessonStart.toDateString();
-        var lessonStartToString = lessonStart.toString();
-        var lessonEnd = new Date(lessonToEdit.end);
-        var lessonEndString = lessonEnd.toDateString();
-        var lessonEndStringUTC = lessonEnd.toUTCString();
-        var lessonEndStringISO = lessonEnd.toISOString();
-        var lessonEndJSON = lessonEnd.toJSON();
-        console.log('lessonStart ' + typeof lessonStart + ' start ' + lessonStart) ;
-        console.log('lessonStartToString ' + typeof lessonStartToString + ' start ' + lessonStartToString) ;
-        console.log('lessonEnd ' + typeof lessonEnd + ' end ' + lessonEnd) ;
-        console.log('lessonEndString ' + typeof lessonEndString + ' end ' + lessonEndString) ;
-        console.log('lessonEndStringUTC ' + typeof lessonEndStringUTC + ' end ' + lessonEndStringUTC) ;
-        console.log('lessonEndStringISO ' + typeof lessonEndStringISO + ' end ' + lessonEndStringISO) ;
-        console.log('lessonEndJSON ' + typeof lessonEndJSON + ' end ' + lessonEndJSON) ;
-//        $scope.activelesson = {id: lessonToEdit.id, start:lessonStartString, end:lessonEndString};
-        $scope.activelesson = lessonToEdit;
+        console.log('lessonToEdit: ' + JSON.stringify(lessonToEdit));
+        $scope.activeLesson = lessonToEdit;
         $scope.activeLessonIndex = index;
         $scope.resetLessonForm();
         $scope.showLessonDetail('Lektion editieren');
+        console.log('activeLesson: ' + JSON.stringify($scope.activeLesson));
+
     }
 
     $scope.newLesson = function() {
